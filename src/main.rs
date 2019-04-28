c286f75b7b5f05338b25be5a03ee3b5075a84ddf
Erich Schroeter <erich.schroeter@gmail.com>
2019-04-28 18:57:46 -0400

    Stubbed out basic gather and check cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs;
+use docopt::Docopt;
+use serde::Deserialize;
+
+const USAGE: &'static str = "
+reqtangle
+
+Usage:
+  reqtangle gather [<paths>...]
+  reqtangle check [<paths>...]
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    cmd_gather: bool,
+    cmd_check: bool,
+    arg_paths: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+    if args.cmd_gather {
+        println!("req gather {}", args.arg_paths.unwrap_or_default());
+        unimplemented!("req gather")
+    } else if args.cmd_check {
+        println!("req check {}", args.arg_paths.unwrap_or_default());
+    }
 }
